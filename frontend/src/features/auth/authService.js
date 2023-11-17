8b231c02bad41e42248bf5a99c29f69c3e688dd2
Michelle Kuwahara <mxk200037@utdallas.edu>
2023-11-17 05:12:32 -0600

    Remove /user in api url

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://website-mern-backend-production.up.railway.app/user";
+const API_URL = "https://website-mern-backend-production.up.railway.app";
 
 // Register user
 const register = async (userData) => {
